7a55d04443966d790915991e5706122f8f4d206e
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-05-02 17:38:38 +0200

    Mac: Install go-jira client.

--- .prefs/mac.sh
+++ .prefs/mac.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Brew some craft packages.
-brew install z ffmpeg ack diff-so-fancy git hub gnu-sed imagemick gradle less pidcat thefuck youtube-dl htop
+brew install z ffmpeg ack diff-so-fancy git hub gnu-sed imagemick gradle less pidcat thefuck youtube-dl htop go-jira
 
 # Phraseapp.
 brew tap phrase/brewed
