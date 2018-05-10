61736af2b746c55486e4da6c13d8ffa4251492bf
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-05-10 10:00:14 +0200

    Mac: Install poppler for separating PDFs.

--- .prefs/mac.sh
+++ .prefs/mac.sh
@@ -7,6 +7,9 @@ set -euo pipefail
 # Brew some craft packages.
 brew install z ffmpeg ack diff-so-fancy git hub gnu-sed imagemick gradle less pidcat thefuck youtube-dl htop go-jira
 
+# Poppler which brings pdfseparate (pdfseparate sample.pdf sample-%d.pdf)
+brew install poppler
+
 # Phraseapp.
 brew tap phrase/brewed
 brew install phraseapp
