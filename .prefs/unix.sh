9bbd6eac807b093ac09e0e979256170f6a135170
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-12-24 21:42:50 +0100

    Make unix.sh able to reexecute.

--- .prefs/unix.sh
+++ .prefs/unix.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
+
+zsh_plugins_path="$HOME/.oh-my-zsh/custom/plugins"
 
 # SpeedRead oh-my-zsh plugin.
-git clone https://github.com/pasky/speedread.git "$ZSH_CUSTOM/plugins/speedread"
+rm -rf "$zsh_plugins_path/speedread"
+git clone git@github.com:pasky/speedread.git "$zsh_plugins_path/speedread"
 
-# AutoSuggestions.
-git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+# AutoSuggestions oh-my-zsh plugin.
+rm -rf "$zsh_plugins_path/zsh-autosuggestions"
+git clone git@github.com:zsh-users/zsh-autosuggestions.git "$zsh_plugins_path/zsh-autosuggestions"
 
 # Fastlane.
 sudo gem install fastlane
 
 # Travis.
 sudo gem install travis
+
+# Clean up.
+sudo gem cleanup
