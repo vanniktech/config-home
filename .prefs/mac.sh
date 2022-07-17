f41c25f927963dd1d0fd4489ffc56f4c43a3d927
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-07-17 13:49:52 +0200

    Mac: Install & use Duti (#232)

--- .prefs/mac.sh
+++ .prefs/mac.sh
@@ -68,7 +68,7 @@ brew install diff-so-fancy
 # Workarounds for Pidcat.
 # https://github.com/JakeWharton/pidcat/issues/180#issuecomment-1124019329
 brew install pyenv
-echo "n" > pyenv install 2.7.18
+echo n | pyenv install 2.7.18 || true
 
 # Pidcat.
 brew install pidcat
@@ -147,6 +147,20 @@ brew install cocoapods || true
 brew install awscli
 brew install docker-credential-helper-ecr
 brew install --cask session-manager-plugin
+
+# Duti. duti -x sh
+brew install duti
+duti -s com.sublimetext.3 .css all
+duti -s com.sublimetext.3 .gradle all
+duti -s com.sublimetext.3 .groovy all
+duti -s com.sublimetext.3 .java all
+duti -s com.sublimetext.3 .json all
+duti -s com.sublimetext.3 .kt all
+duti -s com.sublimetext.3 .kts all
+duti -s com.sublimetext.3 .md all
+duti -s com.sublimetext.3 .sh all
+duti -s com.sublimetext.3 .swift all
+duti -s com.sublimetext.3 .xml all
 
 # Cleaning up.
 brew cleanup
